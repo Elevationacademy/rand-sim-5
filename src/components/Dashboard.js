893b35159abe105179df97aadda681d2397dbb28
danbrud <dannybrudner@users.noreply.github.com>
2019-07-18 18:02:33 +0300

    added component files

---
+++ src/components/Dashboard.js
@@ -0,0 +1,23 @@
+import { React, Component } from 'react';
+
+
+class Dashboard extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      notes: []
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Dashboard
