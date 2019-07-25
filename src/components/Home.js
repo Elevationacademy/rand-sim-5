786b1c3d734b562e4e5e45cd6459ad7d4c7bae8c
danbrud <dannybrudner@users.noreply.github.com>
2019-07-25 12:21:35 +0300

    changed home component to functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import SearchSection from './SearchSection';
 import ImageSection from './ImageSection';
 
-class Home extends Component {
-
-  render() {
-    return (
-      <div>
-        <SearchSection requestPhotos={this.props.requestPhotos} />
-        {this.props.images.length ? <ImageSection requestPhotos={this.props.requestPhotos} page={this.props.page} images={this.props.images} /> : null}
-      </div>
-    );
-  }
+function Home(props) {
+  return (
+    <div>
+      <SearchSection requestPhotos={props.requestPhotos} />
+      {props.images.length ? <ImageSection requestPhotos={props.requestPhotos} page={props.page} images={props.images} /> : null}
+    </div>
+  );
 }
 
 export default Home
